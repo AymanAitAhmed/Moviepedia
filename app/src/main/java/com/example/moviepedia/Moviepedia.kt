2d4527d05f1e55729c9fefe5d4cfab2e066a21e8
Ayman <aitahmed.ayman@etu.uae.ac.ma>
2023-09-30 18:05:58 +0200

    initial commit

--- app/src/main/java/com/example/moviepedia/Moviepedia.kt
+++ app/src/main/java/com/example/moviepedia/Moviepedia.kt
@@ -1,7 +1,4 @@
 package com.example.moviepedia
 
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class Moviepedia : Application()
+class Moviepedia {
+}
--- app/src/main/java/com/example/moviepedia/presentation/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/moviepedia/presentation/splashscreen/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.example.moviepedia.presentation.splashscreen
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
@@ -12,18 +11,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.moviepedia.components.Screens
-import com.example.moviepedia.presentation.movie_list_template.MovieListViewModel
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
-    navController: NavController,
-    viewModel: MovieListViewModel
+    navController: NavController
 ) {
 
     LaunchedEffect(key1 = 1) {
-        viewModel.getCurrentLayoutType()
-        Log.d("SplashScreen", "got the layout type")
+        delay(3000L)
         navController.navigate(Screens.ListsGraph.route)
     }
     Box(
