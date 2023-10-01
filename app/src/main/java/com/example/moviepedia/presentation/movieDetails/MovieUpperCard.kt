e7ee4fca7074aa9353435884eaf4b243a3e0e49e
Ayman <aitahmed.ayman@etu.uae.ac.ma>
2023-10-01 18:10:26 +0200

    added MovieUpperCard.kt

---
+++ app/src/main/java/com/example/moviepedia/presentation/movieDetails/MovieUpperCard.kt
@@ -0,0 +1,168 @@
+package com.example.moviepedia.presentation.movieDetails
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
+import com.example.moviepedia.R
+
+@Composable
+fun MovieUpperCard(
+    imageURL : String,
+    title : String,
+    date : String,
+    duration : Int,
+    releaseState : String,
+    rating : Float,
+    ratingCount : Int,
+    popularity : Float
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        SubcomposeAsyncImage(
+            model = imageURL,
+            contentDescription = "poster",
+            modifier = Modifier
+                .fillMaxWidth(0.3f)
+                .fillMaxHeight(0.95f),
+            contentScale = ContentScale.Crop,
+            loading = {
+                CircularProgressIndicator()
+            }
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(start = 8.dp, end = 8.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = title,
+                fontSize = 15.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+            Text(
+                text = date,
+                fontSize = 11.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+            if (duration != 0){
+                Text(
+                    text = formatDuration(duration),
+                    fontSize = 11.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }else{
+                Text(
+                    text = releaseState,
+                    fontSize = 11.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max)
+                ,
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_star_24),
+                    contentDescription = "rating",
+                    tint = Color(0xFFf5bf03)
+                )
+                Text(
+                    text = "$rating($ratingCount)",
+                    fontSize= 10.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_popularity_24),
+                    contentDescription = "rating",
+                    tint = Color(0xFFf5bf03)
+                )
+                Text(
+                    text = popularity.toString(),
+                    fontSize= 10.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+            }
+        }
+    }
+
+}
+
+
+fun formatDuration(minutes: Int): String {
+    val hours = minutes / 60
+    val remainingMinutes = minutes % 60
+
+    val hoursPart = if (hours > 0) "${hours}h" else ""
+    val minutesPart = if (remainingMinutes > 0) "${remainingMinutes}m" else ""
+
+    return when {
+        hoursPart.isNotEmpty() && minutesPart.isNotEmpty() -> "$hoursPart$minutesPart"
+        hoursPart.isNotEmpty() -> hoursPart
+        minutesPart.isNotEmpty() -> minutesPart
+        else -> "0m"  // If both hours and minutes are 0
+    }
+}
